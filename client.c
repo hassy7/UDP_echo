e6d4fda613231cb3d86ce7514e62e572f396b97c
hassy <hassy@inl.ics.keio.ac.jp>
2017-03-01 15:26:32 +0900

    UDP_complet

--- client.c
+++ client.c
@@ -1,11 +1,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+
+struct echo_msg {
+	unsigned int seq;
+	unsigned int reserve;
+	char msg[32];
+};
 
 int main (int argc, char *argv[]) {
-	int s, count, datalen;
-	in_port_t myport;
-	in_port_t port;           //送信相手のポート番号
+	int s, count, datalen, i;
+	in_port_t port = atoi(argv[2]);           //送信相手のポート番号
 	struct sockaddr_in myskt;
 	struct sockaddr_in skt;   //相手のソケット
 	struct in_addr ipaddr;    //送信相手のIPアドレス
@@ -13,36 +22,44 @@ int main (int argc, char *argv[]) {
 	char sbuf[512];           //送信用のバッファ
 	socklen_t sktlen;
 
-	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
+	struct echo_msg c_msg;
+	c_msg.seq = 0;
 
 	//myportの設定
 
-	bzero (&myskt, sizeof myskt);
-	myskt.sin_family = AF_INET;
-	myskt.sin_port = htons(myport);
-	myskt.sin_addr.s_addr = htonl(INADDR_ANY);
+	//bzero(&myskt, sizeof myskt);
+	//myskt.sin_family = AF_INET;
+	//myskt.sin_port = htons(myport);
+	//myskt.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0) {
-		perror("bind");
-		exit(1);
-	}
 
-	for (;;) {
-		sktlen = sizeof skt;
-		if ((count = recvfrom(s, rbuf, sizeof rbuf, 0, (struct sockaddr *)&skt, &sktlen)) < 0) {
-			perror("recvfrom");
+	for (i = 0; i < 10; i++) {
+		if (i < 9) {
+			printf("Input message : ");
+			fgets(c_msg.msg, 32, stdin);
+		} else {
+			strcpy(c_msg.msg, "FIN\n");
+		}
+		if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+			perror("socket");
 			exit(1);
 		}
+
+		sktlen = sizeof skt;
 
 		skt.sin_family = AF_INET;
 		skt.sin_port = htons(port);
-		skt.sin_addr.s_addr = htonl(ipaddr.s_addr);
-		if ((count = sendto(s, sbuf, datalen, 0, (struct sockaddr *)&skt, sizeof skt)) < 0) {
+		skt.sin_addr.s_addr = inet_addr(argv[1]);
+		if ((count = sendto(s, &c_msg, sizeof c_msg, 0, (struct sockaddr *)&skt, sizeof skt)) < 0) {
 			perror("sendto");
 			exit(1);
 		}
+		printf("sended\n");
+
+		if ((count = recvfrom(s, &c_msg, sizeof c_msg, 0, (struct sockaddr *)&skt, &sktlen)) < 0) {
+			perror("recvfrom");
+			exit(1);
+		}
+		if (strcmp(c_msg.msg, "FIN\n") == 0) break;
 	}
 }
--- server.c
+++ server.c
@@ -1,28 +1,38 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+struct echo_msg {
+	unsigned int seq;
+	unsigned int reserve;
+	char msg[32];
+};
 
 int main (int argc, char *argv[]) {
 	int s, count, datalen;
-	in_port_t myport;
-	in_port_t port;           //送信相手のポート番号
-	struct sockaddr_in myskt;
+	in_port_t port = atoi(argv[1]);           //送信相手のポート番号
+	struct sockaddr_in myskt; //自ソケット
 	struct sockaddr_in skt;   //相手のソケット
 	struct in_addr ipaddr;    //送信相手のIPアドレス
-	char rbuf[512];
+	char rbuf[512];           //受信用バッファ
 	char sbuf[512];           //送信用のバッファ
 	socklen_t sktlen;
+	struct echo_msg s_msg;
 
 	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket");
 		exit(1);
 	}
 
-	//myportの設定
+	//portの設定
 
-	bzero (&myskt, sizeof myskt);
-	myskt.sin_family = AF_INET;
-	myskt.sin_port = htons(myport);
+	bzero(&myskt, sizeof myskt);
+	myskt.sin_family = PF_INET;
+	myskt.sin_port = htons(port);
 	myskt.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0) {
@@ -32,17 +42,22 @@ int main (int argc, char *argv[]) {
 
 	for (;;) {
 		sktlen = sizeof skt;
-		if ((count = recvfrom(s, rbuf, sizeof rbuf, 0, (struct sockaddr *)&skt, &sktlen)) < 0) {
+		printf("waiting\n");
+		if ((count = recvfrom(s, &s_msg, sizeof s_msg, 0, (struct sockaddr *)&skt, &sktlen)) < 0) {
 			perror("recvfrom");
 			exit(1);
 		}
-
-		skt.sin_family = AF_INET;
-		skt.sin_port = htons(port);
-		skt.sin_addr.s_addr = htonl(ipaddr.s_addr);
-		if ((count = sendto(s, sbuf, datalen, 0, (struct sockaddr *)&skt, sizeof skt)) < 0) {
-			perror("sendto");
+		s_msg.seq = s_msg.seq + 1;
+		printf("receive\n");
+		printf("seq : %d, msg : %s\n", s_msg.seq, s_msg.msg);
+		//if ((count = sendto(s, sbuf, datalen, 0, (struct sockaddr *)&skt, sizeof skt)) < 0) {
+		if ((count = sendto(s, &s_msg, sizeof s_msg, 0, (struct sockaddr *)&skt, sizeof skt)) < 0) {
+		perror("sendto");
 			exit(1);
 		}
+		printf("%s\n", s_msg.msg);
+		if (s_msg.seq >= 10 || strcmp(s_msg.msg, "FIN\n") == 0) break;
 	}
+	close(s);
+	exit(1);
 }
